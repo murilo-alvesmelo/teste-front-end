eb44a41b537b7dd21ce33941d6a65a0f3862bea9
murilo-alvesmelo <murilo.melo72@gmail.com>
2023-03-15 10:29:39 -0300

    Integrando API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "./componentes/Banner";
 import Footer from "./componentes/Footer";
 import Cards from "./componentes/Cards";
-
+import axios from "axios";
 
 function App() {
+  const[vinculos, setVinculos] = useState([])
+
+
+  const getVinculos = () =>{
+    axios.get('http://localhost:3001/vinculo')
+    .then(res => {
+      setVinculos(Object.keys(res.data).map(function(key){
+        return res.data[key]
+      }))
+    })
+    .catch(err => console.log(err))
+  }
+  
+  useEffect(() => {
+    getVinculos()
+  },[])
+
+  
   return (
     <div>
       <Banner/>
-      <Cards/>
-      <Cards/>
-      <Cards/>
-      <Cards/>
+      {vinculos.map((vinculo, i) => (
+        <Cards key={i} {...vinculo}/>
+      )
+      )}
       <Footer/>
     </div>
   );
--- src/componentes/Cards/index.js
+++ src/componentes/Cards/index.js
@@ -1,10 +1,55 @@
 import './Cards.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default function Cards(){
+export default function Cards(props){
+
+    const[produtore, setProdutores] = useState([])
+    const[propriedade, setPropriedades] = useState([])
+    const[monitoramento, setMonitoramentos] = useState([])
+    
+    const getProdutores = () =>{
+      axios.get(`http://localhost:3001/produtores/${props.idProdutor}`)
+      .then(res => setProdutores(res.data))
+      .catch(err => console.log(err))
+    }
+    
+    const getPropriedades = () =>{
+        axios.get(`http://localhost:3001/propriedades/${props.idProdutor}`)
+            .then(res => setPropriedades(res.data))
+            .catch(err => console.log(err))
+    }
+
+    const getMonitoramentos = () =>{
+        axios.get(`http://localhost:3001/monitoramentos/${props.idVinculo}`)
+            .then(res => setMonitoramentos(res.data))
+            .catch(err => console.log(err))
+    }
+    useEffect(() => {
+        getProdutores()
+        getPropriedades()
+        getMonitoramentos()
+    },[])
+
+    console.log(monitoramento)
     return(
         <div className='monitoramento'>
             <div className='card'>
-                <h2>Titulo</h2>
+                <div>
+                    <h4>Nome da Propriedade</h4>
+                    <p>{propriedade.nomePropriedade} - {propriedade.numeroCadastroRural}</p>
+                </div>
+                <div>
+                    <h4>Informações do Produtor</h4>
+                    <p>{produtore.nomeProdutor}</p>
+                    <p>{produtore.cpfProdutor}</p>
+                </div>
+                <div>
+                    <h4>Monitoramento</h4>
+                    <p>{monitoramento.dataMonitoramento}</p>
+                    <p>{monitoramento.analista}</p>
+                    <p>{monitoramento.resultado}</p>
+                </div>
             </div>
         </div>
     )
